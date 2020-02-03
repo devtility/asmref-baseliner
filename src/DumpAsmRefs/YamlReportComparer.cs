81197b131704a2f2cd86e9c130f8d5f53114b4dc
gh-devtility <contact@devtility.com>
2020-02-03 20:58:49 +0100

    Added simple report comparer

---
+++ src/DumpAsmRefs/YamlReportComparer.cs
@@ -0,0 +1,97 @@
+﻿// Copyright (c) 2020 Devtility.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the repo root for license information.
+
+using DumpAsmRefs.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace DumpAsmRefs
+{
+    internal class YamlReportComparer
+    {
+        private readonly IFileSystem fileSystem;
+
+        public YamlReportComparer()
+            : this(new FileSystemAbstraction())
+        {
+        }
+
+        internal YamlReportComparer(IFileSystem fileSystem)
+        {
+            this.fileSystem = fileSystem ?? throw new ArgumentNullException(nameof(fileSystem));
+        }
+
+        public bool AreSame(string baselineFilePath, string comparisonFilePath)
+        {
+            if (!fileSystem.FileExists(baselineFilePath))
+            {
+                throw new System.IO.FileNotFoundException(UIStrings.ReportComparer_Error_ReportNotFound, baselineFilePath);
+            }
+
+            if (!fileSystem.FileExists(comparisonFilePath))
+            {
+                throw new System.IO.FileNotFoundException(UIStrings.ReportComparer_Error_ReportNotFound, comparisonFilePath);
+            }
+
+            var source = GetProcessedLines(baselineFilePath);
+            var target = GetProcessedLines(comparisonFilePath);
+
+            if (source.Length != target.Length)
+            {
+                return false;
+            }
+
+            for (int index = 0; index < source.Length; index++)
+            {
+                if (!source[index].Equals(target[index]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private string[] GetProcessedLines(string filePath)
+        {
+            var originalLines = fileSystem.ReadAllLines(filePath);
+            var processed = new List<string>();
+
+            foreach(var line in originalLines)
+            {
+                var processedLine = GetProcessedLine(line);
+                if (processedLine != null)
+                {
+                    processed.Add(processedLine);
+                }
+            }
+
+            return processed.ToArray();
+        }
+
+        internal static string GetProcessedLine(string line)
+        {
+            // Strips comments and trailing whitespace
+            // Note: does not correctly handle comment markers in whitespace
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                return null;
+            }
+
+            var processedLine = line;
+            var startCommentIndex = line.IndexOf('#');
+            if (startCommentIndex > -1)
+            {
+                processedLine = line.Substring(0, startCommentIndex);
+            }
+
+            processedLine = processedLine.TrimEnd();
+
+            if (string.IsNullOrWhiteSpace(processedLine))
+            {
+                return null;
+            }
+
+            return processedLine;
+        }
+    }
+}
---
+++ tests/DumpAsmRefs.Tests/Infrastructure/DummyFileSystem.cs
@@ -0,0 +1,40 @@
+﻿using DumpAsmRefs.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace DumpAsmRefs.Tests.Infrastructure
+{
+    internal class DummyFileSystem : IFileSystem
+    {
+        private readonly Dictionary<string, string> fileToPathMap = new Dictionary<string, string>();
+
+        public void AddFile(string path, string contents)
+        {
+            fileToPathMap[path] = contents;
+        }
+
+        #region IFileSystem implementation
+
+        bool IFileSystem.FileExists(string path)
+        {
+            return fileToPathMap.ContainsKey(path);
+        }
+
+        string[] IFileSystem.ReadAllLines(string path)
+        {
+            return ((IFileSystem)this).ReadAllText(path).Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+        }
+
+        string IFileSystem.ReadAllText(string path)
+        {
+            return fileToPathMap[path];
+        }
+
+        void IFileSystem.WriteAllFile(string path, string contents)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion IFileSystem implementation
+    }
+}
