d9265862d4e2ba607ce6c452344f1bccf3969112
gh-devtility <contact@devtility.com>
2020-04-14 22:14:43 +0100

    e2e: extracted AbstractExeBuildRunner class (#60)
    * Refactored e2e test exe runner

---
+++ tests/DumpAsmRefs.MSBuild.Tests/Infrastructure/AbstractExeBuildRunner.cs
@@ -0,0 +1,85 @@
+﻿// Copyright (c) 2020 Devtility.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the repo root for license information.
+
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Xunit.Abstractions;
+
+namespace DumpAsmRefs.MSBuild.Tests
+{
+    internal abstract class AbstractExeBuildRunner : IBuildRunner
+    {
+        protected AbstractExeBuildRunner(ITestOutputHelper output)
+        {
+            this.Output = output ?? throw new ArgumentNullException(nameof(output));
+        }
+
+        protected ITestOutputHelper Output { get; }
+
+        protected abstract string BinLogFileNamePrefix { get; }
+
+        protected abstract string ExePath { get; }
+
+        protected virtual void InitializeBuild() { /* no-op */ }
+
+        protected abstract string BuildCommandLineArgs(string projectFilePath, string targetName,
+            string binLogFilePath, Dictionary<string, string> additionalProperties);
+
+        BuildChecker IBuildRunner.BuildSingleTarget(string projectFilePath, string targetName,
+            Dictionary<string, string> additionalProperties)
+        {
+            InitializeBuild();
+
+            var projectDir = Path.GetDirectoryName(projectFilePath);
+            var binLogFilePath = Path.Combine(projectDir, $"{BinLogFileNamePrefix}.{targetName}.binlog");
+
+            if (File.Exists(binLogFilePath))
+            {
+                WriteLine($"Deleting existing binlog file: {binLogFilePath}");
+                File.Delete(binLogFilePath);
+            }
+
+            var args = BuildCommandLineArgs(projectFilePath, targetName, binLogFilePath, additionalProperties);
+            WriteLine($"Command line arguments: {args}");
+
+            var executionResult = ExeRunner.Run(ExePath, args);
+            DumpExecutionResult(executionResult);
+
+            File.Exists(binLogFilePath).Should().BeTrue();
+
+            var buildSucceeded = executionResult.Status == ExeRunner.ExecutionStatus.Completed
+                && executionResult.ExitCode == 0;
+
+            var buildChecker = new BuildChecker(buildSucceeded, binLogFilePath);
+            return buildChecker;
+        }
+
+        private void DumpExecutionResult(ExeRunner.ExecutionResult result)
+        {
+            var standardOutput = result.StandardOutput;
+            var standardError = result.StandardError;
+            if (string.IsNullOrEmpty(standardOutput))
+            {
+                standardOutput = "{empty}";
+            }
+            if (string.IsNullOrEmpty(standardError))
+            {
+                standardError = "{empty}";
+            }
+
+            WriteLine($"Exit code: {result.ExitCode}");
+            WriteLine($"Execution status: {result.Status}");
+            WriteLine($"Standard output: {standardOutput}");
+            WriteLine($"Standard error: {standardError}");
+            WriteLine(string.Empty);
+            WriteLine(string.Empty);
+        }
+
+        protected void WriteLine(string message)
+        {
+            Output.WriteLine(message);
+            Console.WriteLine(message);
+        }
+    }
+}
--- tests/DumpAsmRefs.MSBuild.Tests/Infrastructure/DotNetBuildRunner.cs
+++ tests/DumpAsmRefs.MSBuild.Tests/Infrastructure/DotNetBuildRunner.cs
@@ -1,38 +1,32 @@
 ﻿// Copyright (c) 2020 Devtility.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the repo root for license information.
 
-using FluentAssertions;
-using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Text;
 using Xunit.Abstractions;
 
 namespace DumpAsmRefs.MSBuild.Tests
 {
-    internal class DotNetBuildRunner : IBuildRunner
+    internal class DotNetBuildRunner : AbstractExeBuildRunner
     {
         private const string exePath = "dotnet";
 
-        private readonly ITestOutputHelper output;
-
         public DotNetBuildRunner(ITestOutputHelper output)
+            :base(output)
         {
-            this.output = output;
         }
 
-        BuildChecker IBuildRunner.BuildSingleTarget(string projectFilePath, string targetName, Dictionary<string, string> additionalProperties)
+        protected override void InitializeBuild()
         {
             DumpDotNetVersion();
+        }
 
-            var projectDir = Path.GetDirectoryName(projectFilePath);
-            var binLogFilePath = Path.Combine(projectDir, $"msbuild.{targetName}.binlog");
+        protected override string BinLogFileNamePrefix => "dotnet";
 
-            if (File.Exists(binLogFilePath))
-            {
-                WriteLine($"Deleting existing binlog file: {binLogFilePath}");
-                File.Delete(binLogFilePath);
-            }
+        protected override string ExePath => "dotnet";
 
+        protected override string BuildCommandLineArgs(string projectFilePath, string targetName,
+            string binLogFilePath, Dictionary<string, string> additionalProperties)
+        {
             var sb = new StringBuilder();
             sb.Append($"build \"{projectFilePath}\"");
             sb.Append($" -t:{targetName} ");
@@ -48,50 +42,13 @@ namespace DumpAsmRefs.MSBuild.Tests
             }
 
             var args = sb.ToString();
-            WriteLine($"Command line arguments: {args}");
-            var executionResult = ExeRunner.Run(exePath, args);
-            DumpExecutionResult(executionResult);
-
-            File.Exists(binLogFilePath).Should().BeTrue();
-
-            var buildSucceeded = executionResult.Status == ExeRunner.ExecutionStatus.Completed
-                && executionResult.ExitCode == 0;
-
-            var buildChecker = new BuildChecker(buildSucceeded, binLogFilePath);
-            return buildChecker;
+            return args;
         }
 
         private void DumpDotNetVersion()
         {
             var executionResult = ExeRunner.Run(exePath, "--version");
             WriteLine($"dotnet version: {executionResult.StandardOutput}");
-        }
-
-        private void DumpExecutionResult(ExeRunner.ExecutionResult result)
-        {
-            var standardOutput = result.StandardOutput;
-            var standardError = result.StandardError;
-            if (string.IsNullOrEmpty(standardOutput))
-            {
-                standardOutput = "{empty}";
-            }
-            if (string.IsNullOrEmpty(standardError))
-            {
-                standardError = "{empty}";
-            }
-
-            WriteLine($"Exit code: {result.ExitCode}");
-            WriteLine($"Execution status: {result.Status}");
-            WriteLine($"Standard output: {standardOutput}");
-            WriteLine($"Standard error: {standardError}");
-            WriteLine(string.Empty);
-            WriteLine(string.Empty);
-        }
-
-        private void WriteLine(string message)
-        {
-            output.WriteLine(message);
-            Console.WriteLine(message);
         }
     }
 }
