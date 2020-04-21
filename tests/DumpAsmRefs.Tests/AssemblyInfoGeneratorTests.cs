ea38b7ed94d72fbac9161099a7c5c79260666c27
gh-devtility <contact@devtility.com>
2020-04-21 20:49:28 +0100

    Simplify load exception text (#69)
    - previous strings were invalid yaml

--- tests/DumpAsmRefs.Tests/AssemblyInfoGeneratorTests.cs
+++ tests/DumpAsmRefs.Tests/AssemblyInfoGeneratorTests.cs
@@ -92,7 +92,6 @@ namespace DumpAsmRefs.Tests
             // Set up unloadable assembly
             var exceptionFilePath = "c:\\invalid1.dll";
             var expectedException = new BadImageFormatException("foo");
-            var expectedLoadExceptionText = "System.BadImageFormatException. foo";
             testSubject.FilePathToExceptionToThrowMap[exceptionFilePath] = expectedException;
 
             // Set up valid assemblies
@@ -112,7 +111,8 @@ namespace DumpAsmRefs.Tests
             CheckReferencedAssemblies(result[0], typeof(System.Collections.CollectionBase));
 
             result[1].FullPath.Should().Be("c:\\invalid1.dll");
-            result[1].LoadException.Should().Be(expectedLoadExceptionText);
+            result[1].LoadException.Should().ContainAll("System.BadImageFormatException", UIStrings.InfoGenerator_NotADotNetAssembly);
+
             result[1].ReferencedAssemblies.Should().BeNull();
 
             result[2].FullPath.Should().Be("c:\\valid2.dll");
